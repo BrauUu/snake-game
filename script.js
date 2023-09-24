308db15179e38f867855b117b7e79762deadd410
BrauUu <brayanfrank.sm@gmail.com>
2023-09-24 16:05:05 -0300

    finish version 1.0

--- script.js
+++ script.js
@@ -7,15 +7,10 @@ const Snake = {
         Snake.axisX = x
         Snake.axisY = y
     },
-    updateViewDirection(viewDirection){
+    updateViewDirection(viewDirection) {
         Snake.viewDirection = viewDirection
     },
-    tailPositions: [
-        {axisX: 20, axisY: 20},
-        {axisX: 40, axisY: 20},
-        {axisX: 40, axisY: 20},
-        {axisX: 40, axisY: 20}
-    ],
+    tailPositions: [],
     points: 1
 }
 
@@ -28,63 +23,138 @@ const Fruit = {
     },
 }
 
-//TODO: Timer do movimento reinicia ao fazer curva (Pode ser que ao virar a cobra, ela apenas vire, sem andar para o lado)
+let eventOnProgress = false
 
-
-window.setInterval(goStraight, 500)
+const intervalID = window.setInterval(goStraight, 500)
 
 window.addEventListener('load', () => {
     renderGame()
 })
 
-window.addEventListener('keydown', async (event) => {
+
+window.addEventListener('keydown', (event) => {
+
+    if (eventOnProgress) {
+        event.preventDefault()
+        return
+    }
+
+    eventOnProgress = true
+
     const key = event.key
-    await handleKeyPressed(key)
+    handleKeyPressed(key)
 })
 
-function goStraight(){
-    handleKeyPressed(`Arrow${Snake.viewDirection}`, true)
+function goStraight() {
+
+    const Moves = {
+        'Up': { axisX: 0, axisY: -1 },
+        'Right': { axisX: 1, axisY: 0 },
+        'Down': { axisX: 0, axisY: 1 },
+        'Left': { axisX: -1, axisY: 0 },
+    }
+
+    const { axisX, axisY} = Moves[Snake.viewDirection]
+
+    handleMovement(axisX, axisY)
+
+    eventOnProgress = false
 }
 
-async function handleMovement(x, y){
+function handleMovement(x, y) {
 
-    let { axisX, axisY, color, updatePosition , tailPositions, points} = Snake
-
+    let { axisX, axisY, color, updatePosition, tailPositions } = Snake
     let tempAxisX = axisX + x * 20
     let tempAxisY = axisY + y * 20
 
-    tailPositions.unshift({axisX: axisX, axisY: axisY})
+    
+    updatePosition(tempAxisX, tempAxisY)
+    if(handleCollision()){
+        return
+    }
+    
+    tailPositions.unshift({ axisX: axisX, axisY: axisY })
     const blank = tailPositions.pop()
 
-    updatePosition(tempAxisX, tempAxisY)
-    handleCollision()
-    
     drawRectangle(blank.axisX, blank.axisY, "#eff0f1")
     drawRectangle(tempAxisX, tempAxisY, color)
 
-        for(block of tailPositions){
-           drawRectangle(block.axisX, block.axisY, color)
-        }
+    for (block of tailPositions) {
+        drawRectangle(block.axisX, block.axisY, color)
+    }
 }
 
-function handleCollision(){
+function handleCollision() {
 
-    let { axisX, axisY, tailPositions} = Snake
+    let { axisX, axisY, tailPositions } = Snake
 
-    //TODO: Game over
-    if(hasColidedWithTail(axisX, axisY, tailPositions)) {
-        console.log('colisão com o rabo')
+    if (hasColidedWithTail(axisX, axisY, tailPositions)) {
+        return gameOver()
+    }
+    else if(hasColidedWithFruit(axisX, axisY)){  
+        Snake.points += 1
+        hasGotFruit()
+        generateFruit()
+    }
+    else if(hasColidedWithWalls(axisX, axisY)){
+        return gameOver()
     }
 
-    //TODO: Ao colidir com fruta + pontuação
+    return false
+}
 
-    //TODO: Ao colidir com parede
+function gameOver(){
+    let canvas = document.querySelector("#canvas");
+    if (canvas.getContext) {
+        let ctx = canvas.getContext("2d");
+        ctx.font = "30px Comic Sans MS";
+        ctx.fillStyle = "red";
+        ctx.textAlign = "center";
+        ctx.fillText("Game Over", canvas.width/2, canvas.height/2); 
+    }
+
+    window.clearInterval(intervalID)
+    return true
+}
+
+function hasColidedWithWalls(axisX, axisY){
+    const Walls = {
+        'axisX' : {
+            'min': 0,
+            'max': 400
+        },
+        'axisY' : {
+            'min': 0,
+            'max': 300
+        }
+    }
+    if(axisX < Walls.axisX.min || axisX >= Walls.axisX.max || axisY < Walls.axisY.min || axisY >= Walls.axisY.max){
+        return true
+    }
+
+    return false
+}
+
+function hasGotFruit(){
+    Snake.tailPositions.push({})
+}
+
+function hasColidedWithFruit(axisX, axisY) {
+
+    const fruitAxisX = Fruit.axisX
+    const fruitAxisY = Fruit.axisY
+
+    if (fruitAxisX === axisX && fruitAxisY === axisY) {
+        return true
+    }
+
+    return false
 
 }
 
-function hasColidedWithTail(axisX, axisY, tailPositions){
-    for(square of tailPositions){
-        if(square.axisX === axisX && square.axisY === axisY){
+function hasColidedWithTail(axisX, axisY, tailPositions) {
+    for (square of tailPositions) {
+        if (square.axisX === axisX && square.axisY === axisY) {
             return true
         }
     }
@@ -92,46 +162,42 @@ function hasColidedWithTail(axisX, axisY, tailPositions){
     return false
 }
 
-async function handleKeyPressed(key, isAutomatic = false){
+function handleKeyPressed(key) {
 
-    const {viewDirection, updateViewDirection} = Snake
-    console.log(key, isAutomatic)
-    const moves = {
+    const { viewDirection, updateViewDirection } = Snake
+
+    const Keys = {
         ArrowUp() {
-            if((viewDirection != 'Down' && viewDirection != 'Up') || isAutomatic){
-                handleMovement(0,-1)
+            if ((viewDirection != 'Down' && viewDirection != 'Up')) {
                 updateViewDirection("Up")
             }
         },
         ArrowRight() {
-            if((viewDirection != 'Left' && viewDirection != 'Right') || isAutomatic){
-                handleMovement(1,0)
+            if ((viewDirection != 'Left' && viewDirection != 'Right')) {
                 updateViewDirection("Right")
             }
         },
         ArrowDown() {
-            if((viewDirection != 'Up' && viewDirection != 'Down') || isAutomatic){
-                handleMovement(0,1) 
+            if ((viewDirection != 'Up' && viewDirection != 'Down')) {
                 updateViewDirection("Down")
-            }      
+            }
         },
         ArrowLeft() {
-            if((viewDirection != 'Right' && viewDirection != 'Left') || isAutomatic){
-                handleMovement(-1,0)
+            if ((viewDirection != 'Right' && viewDirection != 'Left')) {
                 updateViewDirection("Left")
-            }    
+            }
         }
     }
 
-    const move = moves[key]
+    const direction = Keys[key]
 
-    if(move){
-        await move()
+    if (direction) {
+        direction()
     }
 
 }
 
-function renderGame(){
+function renderGame() {
 
     let { axisX, axisY, color } = Snake
 
@@ -139,7 +205,7 @@ function renderGame(){
     generateFruit()
 }
 
-function generateFruit(){
+function generateFruit() {
 
     const color = "#FF00FF"
 
@@ -148,7 +214,7 @@ function generateFruit(){
 
     let axisX = Math.random() * (maxX - 0)
     axisX = axisX - (axisX % 20)
-    
+
     let axisY = Math.random() * (maxY - 0)
     axisY = axisY - (axisY % 20)
 
